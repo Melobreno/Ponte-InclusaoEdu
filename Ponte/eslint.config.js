7b5901f39abdf414df279448b8d58a359a529265
vloliveira <vitorloliveira96@gmail.com>
2024-07-23 23:29:47 -0300

    criação da funcionalidades na LPage

--- Ponte/eslint.config.js
+++ Ponte/eslint.config.js
@@ -1,13 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import { fixupConfigRules } from "@eslint/compat";
+import pluginReact from "eslint-plugin-react";
 
 
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
+  pluginReact.configs.flat.recommended,
 ];
