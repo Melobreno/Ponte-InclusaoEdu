c0dfc820a75acdde999b69aadc855f0c86645232
vloliveira <vitorloliveira96@gmail.com>
2024-07-28 21:15:13 -0300

    feat: atualização de funcionalidades para landing page

--- Ponte/src/Pages/Funcionalidades/Funcionalidades.jsx
+++ Ponte/src/Pages/Funcionalidades/Funcionalidades.jsx
@@ -1,38 +1,77 @@
-import { Titulo, Opcoes } from './FuncionalidadesStyle';
+import { Titulo, Opcoes, Fundo, Section } from './FuncionalidadesStyle';
 import imgFun from '../../Assets/img/Funcionalidades.svg';
 import imgMais from '../../Assets/icons/+.svg';
+// import imgFundo from '../../Assets/img/EfeitosFuncion.png';
 
 function Funcionalidades() {
   return (
-    <section>
-      <Titulo>
-        <h1>FUNCIONALIDADES</h1>
-      </Titulo>
+    <Section>
+      <Fundo>
+        <Titulo>
+          <h1>FUNCIONALIDADES</h1>
+        </Titulo>
+        <Opcoes>
+          <img src={imgFun} alt="Img Funcionalidades" />
+          <div>
+            <ul>
+              <li>
+                <details>
+                  <summary>
+                    Conexão entre pais e professores{' '}
+                    <img className="imgMais" src={imgMais} alt="" />
+                  </summary>
 
-      <Opcoes>
-        <img src={imgFun} alt="Img Funcionalidades" />
-        <div>
-          <ul>
-            <li>
-              <a href="">Conexão entre pais e professores</a>
-              <img className="imgMais" src={imgMais} alt="" />
-            </li>
-            <li>
-              <a href="">Mapeamento de profissionais</a>
-              <img className="imgMais" src={imgMais} alt="" />
-            </li>
-            <li>
-              <a href=""> Acompanhamento contínuo</a>
-              <img className="imgMais" src={imgMais} alt="" />
-            </li>
-            <li>
-              <a href="">Documentação do aluno</a>
-              <img className="imgMais" src={imgMais} alt="" />
-            </li>
-          </ul>
-        </div>
-      </Opcoes>
-    </section>
+                  <p>
+                    A nossa ferramenta vai permitir que a comunicação entre os
+                    profissionais e os responsáveis aconteça de forma organizada
+                    e simplicifada.
+                  </p>
+                </details>
+              </li>
+
+              <li>
+                <details>
+                  <summary>
+                    Mapeamento de profissionais
+                    <img className="imgMais" src={imgMais} alt="" />
+                  </summary>
+                  <p>
+                    Forneceremos um mapeamento de todos os profissionais e
+                    instituições de ensino que utilizam nossa ferramenta.
+                  </p>
+                </details>
+              </li>
+
+              <li>
+                <details>
+                  <summary>
+                    Acompanhamento contínuo{' '}
+                    <img className="imgMais" src={imgMais} alt="" />
+                  </summary>
+                  <p>
+                    A ponte vai fazer com que a sua criança receba um
+                    acompanhamento continuado e personalizado
+                  </p>
+                </details>
+              </li>
+
+              <li>
+                <details>
+                  <summary>
+                    Documentação do aluno{' '}
+                    <img className="imgMais" src={imgMais} alt="" />
+                  </summary>
+                  <p>
+                    Todo o desenvolvimento e progresso realizado da sua criança
+                    ficará salvo e disponível como arquivo PDF.
+                  </p>
+                </details>
+              </li>
+            </ul>
+          </div>
+        </Opcoes>
+      </Fundo>
+    </Section>
   );
 }
 export default Funcionalidades;
