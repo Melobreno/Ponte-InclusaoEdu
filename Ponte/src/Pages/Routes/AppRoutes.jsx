c94782d4a281266c4ce1b6335d4364c6efd716c4
vloliveira <vitorloliveira96@gmail.com>
2024-08-01 21:19:47 -0300

    feat: produção da página de Login

--- Ponte/src/Pages/Routes/AppRoutes.jsx
+++ Ponte/src/Pages/Routes/AppRoutes.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../Home";
+import Login from "../Login/Login";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
   );
