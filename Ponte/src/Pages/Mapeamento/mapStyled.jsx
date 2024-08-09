33e8e729391fd5e4a4b05ed34cf39a2e4499fd4c
Ronson Leon <ronsonleonsilva@gmail.com>
2024-08-08 21:44:51 -0300

    Feat: criacao da page mapeamento

---
+++ Ponte/src/Pages/Mapeamento/mapStyled.jsx
@@ -0,0 +1,118 @@
+import styled from "styled-components";
+
+export const Map = styled.div`
+display: flex;
+padding: 1rem;
+justify-content: center;
+
+
+
+
+.busca{
+    font-size: x-large;
+    font-family: 'montserrat';
+
+}
+
+.nome{
+    font-size: large;
+    margin: 0.2rem;
+    font-family: 'montserrat';
+    
+    
+}
+
+.ocupacao{
+    font-size: small;
+    margin: 0.2rem;
+    font-family: 'montserrat';
+}
+
+.info1{
+  height: 10rem;
+  display: flex;
+  margin-top: 1rem;
+  margin-left: 1rem;
+  
+}
+
+.info2{
+    display: flex;
+   flex-direction: column;
+   height: 10rem;
+   width: 26rem;
+   justify-content: center;
+   align-items: center;
+}
+
+.container3{
+    height: 18rem;
+    width: 40rem;
+    border: 1px solid gray;
+    border-radius: 20px;
+    margin-top: 1rem;
+    display: flex;
+    flex-direction: column;
+    
+    font-family: 'montserrat';
+   
+   
+    
+    
+
+
+}
+.container2{
+    
+    height: 18rem;
+    width: 40rem;
+    border: 1px solid gray;
+    border-radius: 20px;
+    margin-top: 1rem;
+    display: flex;
+    flex-direction: column;
+    
+    font-family: 'montserrat'
+
+
+}
+
+.container1{
+display: flex;
+width: 14rem;
+height: 24rem;
+border: 1.5px solid gray;
+flex-direction: column;
+border-radius: 20px;
+margin: 3rem;
+font-family: 'montserrat';
+
+
+};
+
+ .input{
+   display: flex ;
+   padding: 0.5rem;
+margin: 0.3rem;  
+
+};
+
+.texto{
+display: flex;
+margin-left: 1rem;
+};
+
+.info1 .dados{
+    display: flex;
+    flex-direction: column;
+    margin-top: 2rem;
+    margin-left: 3rem;
+};
+    
+`
+export const Foto1 = styled.div`
+
+`;
+export const Foto2 = styled.div`
+
+`;
