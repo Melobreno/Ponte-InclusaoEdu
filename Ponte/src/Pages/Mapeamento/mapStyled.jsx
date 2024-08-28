59d0ca129ec488929772898fdaa9a5f3910bb44b
Ronson Leon <ronsonleonsilva@gmail.com>
2024-08-28 11:52:02 -0300

    Feat: responsividade e funcionalidades da pagina mapeamento

--- Ponte/src/Pages/Mapeamento/mapStyled.jsx
+++ Ponte/src/Pages/Mapeamento/mapStyled.jsx
@@ -39,17 +39,6 @@ export const Map = styled.div`
     margin-left: 1rem;
   }
 
-  .container3 {
-    height: 18rem;
-    width: 40rem;
-    border: 1px solid gray;
-    border-radius: 20px;
-    margin-top: 2rem;
-    display: flex;
-    flex-direction: column;
-
-    font-family: "montserrat";
-  }
   .container2 {
     height: 18rem;
     width: 40rem;
@@ -58,22 +47,19 @@ export const Map = styled.div`
     margin-top: 2rem;
     display: flex;
     flex-direction: column;
-
     font-family: "montserrat";
   }
 
   .container1 {
     display: flex;
-    width: 12rem;
-    height: 28rem;
+    width: 18rem;
+    height: 17rem;
     border: 1.5px solid gray;
     flex-direction: column;
     border-radius: 20px;
-    margin-top: 4rem;
     margin-left: 1rem;
     font-family: "montserrat";
     padding: 0.4rem;
-    margin-left: 1rem;
   }
 
   .input {
@@ -81,11 +67,12 @@ export const Map = styled.div`
     padding: 0.5rem;
     margin: 0.3rem;
     margin-left: 1rem;
+    gap: 2px;
   }
 
   .texto {
     display: flex;
-    margin-left: 1.5rem;
+    margin-left: 1.6rem;
   }
 
   .info1 .dados {
@@ -106,6 +93,122 @@ export const Map = styled.div`
     padding: 0.8rem;
     text-align: center;
   }
+
+  .button .botao {
+    font-size: 1.04rem;
+    font-family: "Montserrat", sans-serif;
+    font-weight: 700;
+    background-color: #25a6ce;
+    border: none;
+    border-radius: 1.4rem;
+    padding: 0.33rem 0.9rem;
+    color: #ffff;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    min-width: 7.7rem;
+  }
+
+  @media (max-width: 1440px) {
+    .info2 {
+      width: 22rem;
+    }
+
+    .container3,
+    .container2 {
+      width: 35rem;
+    }
+
+    .container1 {
+      width: 12rem;
+      height: 25rem;
+    }
+
+    .card {
+      height: 38rem;
+      width: 35rem;
+    }
+
+    .busca {
+      font-size: large;
+    }
+
+    .nome {
+      font-size: medium;
+    }
+
+    .ocupacao {
+      font-size: x-small;
+    }
+  }
+
+  @media (max-width: 1024px) {
+    .info2 {
+      width: 20rem;
+    }
+
+    .container3,
+    .container2 {
+      width: 30rem;
+    }
+
+    .container1 {
+      width: 10rem;
+      height: 22rem;
+    }
+
+    .card {
+      height: 35rem;
+      width: 30rem;
+    }
+
+    .busca {
+      font-size: large;
+    }
+
+    .nome {
+      font-size: medium;
+    }
+
+    .ocupacao {
+      font-size: x-small;
+    }
+  }
+
+  @media (max-width: 425px) {
+    flex-direction: column;
+    align-items: center;
+
+    .info1,
+    .info2 {
+      flex-direction: column;
+      margin-left: 0;
+      width: 100%;
+    }
+
+    .container3,
+    .container2,
+    .container1,
+    .card {
+      width: 100%;
+      height: auto;
+    }
+
+    .container1 {
+      margin-left: 0;
+      margin-top: 2rem;
+    }
+
+    .card {
+      height: auto;
+    }
+
+    .busca,
+    .nome,
+    .ocupacao {
+      font-size: smaller;
+    }
+  }
 `;
+
 export const Foto1 = styled.div``;
 export const Foto2 = styled.div``;
