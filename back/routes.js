949bc73f2c0b7a6db5e473727c61fd7ba5d0dbc1
Ronson Leon <ronsonleonsilva@gmail.com>
2024-08-29 12:19:13 -0300

    Fix: resposividade cadastros e funcionalidades mapeamento

--- back/routes.js
+++ back/routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const insertUserController = require("./controller/insertUserController");
-const loginUserController = require("./controller/loginUserController");
-const insertDocLink = require("./controller/insertDocLinkController");
-const showDocController = require("./controller/showDocController");
-const deleteDocController = require("./controller/deleteDocController");
+const insertUserController = require("./controller/users/insertUserController");
+const loginUserController = require("./controller/users/loginUserController");
+const insertDocLink = require("./controller/document/insertDocLinkController");
+const showDocController = require("./controller/document/showDocController");
+const deleteDocController = require("./controller/document/deleteDocController");
 const showCepController = require("./controller/mapeamento/showCepController");
 
 router.post("/adicionar", insertUserController.insertUser);
