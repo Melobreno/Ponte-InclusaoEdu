5c366d073e1b60a8d064b370c88d6d9e8834733e
Breno Melo <bigm@discente.ifpe.edu.br>
2024-08-29 22:16:43 -0300

    fix: Tabelas do Banco de dados.

--- back/prisma/migrations/20240830010703_message/migration.sql
+++ back/prisma/migrations/20240830010703_message/migration.sql
@@ -2,11 +2,7 @@
   Warnings:
 
   - You are about to drop the `atividade` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `message` table. If the table is not empty, all the data it contains will be lost.
 
 */
 -- DropTable
 DROP TABLE `atividade`;
-
--- DropTable
-DROP TABLE `message`;
