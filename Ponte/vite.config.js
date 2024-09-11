80b2680bd1f8d37481cebbe1a19e2143455c6277
Breno Melo <bigm@discente.ifpe.edu.br>
2024-09-11 10:38:27 -0300

    Arquivos de Build

--- Ponte/vite.config.js
+++ Ponte/vite.config.js
@@ -12,4 +12,20 @@ export default defineConfig({
     port: 3000,
     host: true,
   },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
