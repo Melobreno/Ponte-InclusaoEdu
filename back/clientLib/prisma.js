7ca9818e7a063a9fd1cd3478134fac5892293b37
Breno Melo <bigm@discente.ifpe.edu.br>
2024-09-26 13:44:00 -0300

    prod back

--- back/clientLib/prisma.js
+++ back/clientLib/prisma.js
@@ -4,4 +4,13 @@ const prisma = new PrismaClient({
   log: ["query"],
 });
 
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
 module.exports = prisma;
